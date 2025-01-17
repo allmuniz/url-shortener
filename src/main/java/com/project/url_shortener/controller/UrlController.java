18be55d00dc004459becd2489f11918180d6c83a
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-01-17 11:55:59 -0300

    feat: adding requests

--- src/main/java/com/project/url_shortener/controller/UrlController.java
+++ src/main/java/com/project/url_shortener/controller/UrlController.java
@@ -1,17 +1,58 @@
 package com.project.url_shortener.controller;
 
 import com.project.url_shortener.dto.ShortenUrlRequest;
+import com.project.url_shortener.dto.ShortenUrlResponse;
+import com.project.url_shortener.entities.Url;
+import com.project.url_shortener.repositories.UrlRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.time.LocalDateTime;
 
 @RestController
 public class UrlController {
 
-    @PostMapping("/shorten-url")
-    public ResponseEntity<Void> shortenUrl(@RequestBody ShortenUrlRequest request) {
+    private final UrlRepository urlRepository;
 
-        return ResponseEntity.ok().build();
+    public UrlController(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    @PostMapping("/shorten-url")
+    public ResponseEntity<ShortenUrlResponse> shortenUrl(@RequestBody ShortenUrlRequest request,
+                                           HttpServletRequest servletRequest) {
+
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        }while (urlRepository.existsById(id));
+
+        urlRepository.save(new Url(id, request.url(), LocalDateTime.now().plusMinutes(1)));
+
+        var redirectUrl = servletRequest.getRequestURI().toString().replace("shorten-url", id);
+
+        return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirect(@PathVariable("id") String id) {
+
+        var url = urlRepository.findById(id);
+
+        if (url.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
     }
 }
---
+++ src/main/java/com/project/url_shortener/dto/ShortenUrlResponse.java
@@ -0,0 +1,4 @@
+package com.project.url_shortener.dto;
+
+public record ShortenUrlResponse(String url) {
+}
